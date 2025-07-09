b8aa228dadda50ab93dca8c090f75368be14f3ab
Suryansh2188 <suryamail4440@gmail.com>
2025-07-09 22:49:08 +0530

    update cors

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,10 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
