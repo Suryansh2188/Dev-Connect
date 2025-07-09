2cc4a435bb7d4471c7ef54d523938a5b507deca6
Suryansh2188 <suryamail4440@gmail.com>
2025-07-09 22:53:31 +0530

    update server

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://dev-connect-frontend-juyb.onrender.com',
   credentials: true
 }));
 app.use(express.json());
